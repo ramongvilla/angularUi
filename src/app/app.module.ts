fef779b33f0405f76dbe85651297d32d14003fe9
ramon <ramong.rv1@gmail.com>
2021-07-06 19:48:46 -0500

    basico de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './components/basico/personas/personas.component';
+import { PersonaComponent } from './components/basico/persona/persona.component';
+import { FormsModule } from '@angular/forms';
+import { CalculadoraComponent } from './components/basico/calculadora/calculadora.component';
+import { ListadoPersonasComponent } from './components/directivas/listado-personas/listado-personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent,
+    CalculadoraComponent,
+    ListadoPersonasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
